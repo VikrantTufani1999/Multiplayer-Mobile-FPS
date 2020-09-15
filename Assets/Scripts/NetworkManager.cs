d0cda31c9ca087b3a1a5026b2a2a8b2c1f0223d2
Vikrant Tufani <8052007294vicky@gmail.com>
2020-09-15 23:15:08 +0530

    Updated 15.09.2020

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("Connection Status")]
+    public Text connectionStatusText;
+
+    [Header("Login UI Panel")]
+    public InputField playerNameInput;
+    public GameObject Login_UI_Panel;
+
+    [Header("GameOptions UI Panel")]
+    public GameObject GameOptions_UI_Panel;
+
+    [Header("Create Room UI Panel")]
+    public GameObject CreateRoom_UI_Panel;
+    public InputField roomNameInputField;
+
+    public InputField maxPlayerInputField;
+
+    [Header("Inside Room UI Panel")]
+    public GameObject InsideRoom_UI_Panel;
+
+    [Header("RoomList UI Panel")]
+    public GameObject RoomList_UI_Panel;
+    public GameObject roomListEntryPrefab;
+    public GameObject roomListParentGameObject;
+
+    [Header("Join Random Room UI Panel")]
+    public GameObject JoinRandomRoom_UI_Panel;
+
+    private Dictionary<string, RoomInfo> cachedRoomList;
+    private Dictionary<string, GameObject> roomListGameObjects;
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatePanel(Login_UI_Panel.name);
+
+        cachedRoomList = new Dictionary<string, RoomInfo>();
+        roomListGameObjects = new Dictionary<string, GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        connectionStatusText.text = " Connection Status: " + PhotonNetwork.NetworkClientState;
+    }
+
+    #endregion
+    
+    #region UI Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if(!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.Log("Player Name is invalid!");
+        }
+    }
+
+    public void OnCreateRoomButtonClicked()
+    {
+        string roomName = roomNameInputField.text;
+
+        if(string.IsNullOrEmpty(roomName))
+        {
+            roomName = "Room " + Random.Range(1000, 10000);
+        }
+
+        RoomOptions roomOptions = new RoomOptions();
+
+        roomOptions.MaxPlayers = (byte)int.Parse(maxPlayerInputField.text);
+
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        ActivatePanel(GameOptions_UI_Panel.name);
+    }
+
+    public void OnShowRoomListButtonClicked()
+    {
+        if(!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+        ActivatePanel(RoomList_UI_Panel.name);
+    }
+
+    public void OnBackButtonClicked()
+    {
+        if(PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+        ActivatePanel(GameOptions_UI_Panel.name);
+    }
+    #endregion
+
+    #region Photon Callbacks
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to the internet.");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " Connected to Photon. ");
+        ActivatePanel(GameOptions_UI_Panel.name);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + " Is Created.");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " Joined To " + PhotonNetwork.CurrentRoom.Name);
+        ActivatePanel(InsideRoom_UI_Panel.name);    
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        ClearRoomListView();    
+
+        foreach(RoomInfo room in roomList)
+        {
+            Debug.Log(room.Name);
+            if(!room.IsOpen || !room.IsVisible || room.RemovedFromList)
+            {
+                if(cachedRoomList.ContainsKey(room.Name))
+                {
+                    cachedRoomList.Remove(room.Name);
+                }
+            }
+            else
+            {
+                // update cachedRoom List
+                if(cachedRoomList.ContainsKey(room.Name))
+                {
+                    cachedRoomList[room.Name] = room;
+                }
+                // add the new room to the cached room List 
+                else
+                {
+                    cachedRoomList.Add(room.Name, room);
+                }
+            }
+        }
+
+        foreach(RoomInfo room in cachedRoomList.Values)
+        {
+            GameObject roomListEntryGameObject = Instantiate(roomListEntryPrefab);
+            roomListEntryGameObject.transform.SetParent(roomListParentGameObject.transform);
+            roomListEntryGameObject.transform.localScale = Vector3.one;
+
+            roomListEntryGameObject.transform.Find("RoomNameText").GetComponent<Text>().text = room.Name;
+            roomListEntryGameObject.transform.Find("RoomPlayersText").GetComponent<Text>().text = room.PlayerCount + " / " + room.MaxPlayers;
+            roomListEntryGameObject.transform.Find("JoinRoomButton").GetComponent<Button>().onClick.AddListener(()=>OnJoinRoomButtonClicked(room.Name));
+
+            roomListGameObjects.Add(room.Name, roomListEntryGameObject);
+        }
+    }
+
+    /*public override void OnLeftLobby()
+    {
+        ClearRoomListView();
+        cachedRoomList.Clear();
+    }*/
+    #endregion
+
+    #region Private Methods
+
+    void OnJoinRoomButtonClicked(string _roomName)
+    {
+        if(PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
+        PhotonNetwork.JoinRoom(_roomName);
+    }
+
+    void ClearRoomListView()
+    {
+        foreach (var roomListGameObject in roomListGameObjects.Values)
+        {
+            Destroy(roomListGameObject);
+        }
+
+        roomListGameObjects.Clear();
+    }
+    #endregion
+
+    #region
+
+    public void ActivatePanel(string panelToBeActivated)
+    {
+        Login_UI_Panel.SetActive(panelToBeActivated.Equals(Login_UI_Panel.name));
+        GameOptions_UI_Panel.SetActive(panelToBeActivated.Equals(GameOptions_UI_Panel.name));
+        CreateRoom_UI_Panel.SetActive(panelToBeActivated.Equals(CreateRoom_UI_Panel.name));
+        InsideRoom_UI_Panel.SetActive(panelToBeActivated.Equals(InsideRoom_UI_Panel.name));
+        RoomList_UI_Panel.SetActive(panelToBeActivated.Equals(RoomList_UI_Panel.name));
+        JoinRandomRoom_UI_Panel.SetActive(panelToBeActivated.Equals(JoinRandomRoom_UI_Panel.name));
+    }
+
+    #endregion
+}
